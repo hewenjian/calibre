c6eef8f07a12d1d297ac177867fbd7d5f484bb86
hewenjian <396735079@qq.com>
2023-10-29 12:24:54 +0800

    脚本：可以通过主程序改“语言”

--- calibre-develop.bat
+++ calibre-develop.bat
@@ -3,5 +3,6 @@ set PATH=D:\Program Files\Calibre Portable_Dev\Calibre;%PATH%
 set CALIBRE_DEVELOP_FROM=D:\Code\tmp\calibre\src
 
 calibre-debug -w "D:\Code\tmp\OneDrive\On China by Henry A. Kissinger (z-lib.org).epub"
+::calibre-debug -g
 
 ::pause
